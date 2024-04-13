278ba5ba32495cc9438ca5dde784f8565b4822a8
SwiftyKey <slipperyfish9@gmail.com>
2024-04-13 16:38:18 +0500

    changed Controllers/ModellingController

--- Controllers/ModellingController/__init__.py
+++ Controllers/ModellingController/__init__.py
@@ -1,9 +1,20 @@
+from Models.Generators import ARandomNumberGenerator
 from Models.Modelling import ModellingParameters, Modelling
+from Models.Estimations import IEstimation
 
 
 class ModellingController:
-    def __init__(self, parameters: ModellingParameters):
-        self._modelling = Modelling(parameters)
+    def __init__(self,
+                 generator: ARandomNumberGenerator,
+                 estimations: list[IEstimation],
+                 M: int,
+                 N: int,
+                 truthValue: float):
+        self._modelling = Modelling(ModellingParameters(generator,
+                                                        estimations,
+                                                        M,
+                                                        N,
+                                                        truthValue))
 
     def EstimateMSE(self):
         return self._modelling.EstimateMSE()
